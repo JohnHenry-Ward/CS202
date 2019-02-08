00356defc3752f6b8535fa562906d364512e5237
JohnHenry <johnhenry514@gmail.com>
2019-02-08 15:58:09 -0800

    initial commit

---
+++ Lesson 12/js/script.js
@@ -0,0 +1,84 @@
+//comment
+/*
+comment
+*/
+
+//Statements
+// window.alert('Hello World');
+
+//print, output info
+// window.alert('Enter your name...');
+// document.write('Enter your email');
+// prompt('What is your name?');
+// confirm('Click Here');
+// console.log('In console now');
+// parseInt(prompt('Enter your age'));
+// element.innerHTML;
+
+// Variables
+// var age = 25;
+// var text = ' years old';
+// alert(age + text);
+
+// var name = prompt('What is your name?');
+// alert("Hello " + name);
+
+//Concatenation
+// document.write('<h2> Hello ' + name + '</h2>');
+
+// var date = Date();
+// document.write(date);
+
+// var userColor = prompt('Favorite Color');
+// document.bgColor = userColor; //doesn't work with bootstrap
+
+//Datatypes
+//Integer, strings, boolean, objects, arrays
+
+// var num = 5;
+// var str = 'word'
+// var bool = true;
+// var obj = {
+//     Name: 'JohnHenry',
+//     Age: 19,
+//     Favorite_Color: 'Green',
+// }
+// var arr = ['blue', 'green', 'yellow'];
+
+// document.write(obj.Name);
+
+//Functions
+
+// function myFunc(num){
+//     document.write('The magic number is... ' + num);
+// }
+// myFunc(6);
+
+//function doStuff(){
+    // document.getElementById('java').innerHTML = 'Hello';
+    
+    // var x = document.getElementById('java');
+    // x.style.color = 'red';
+
+    // document.querySelector('java').style.color = 'blue';
+
+//}
+
+//Events
+//click, load, resize, mouseover, blur, change, mouseout, keydown
+
+//EventListener
+
+var myVar = document.querySelector('.text-center');
+myVar.addEventListener('click', function(){
+    document.getElementById('java').innerHTML = 'Hello';
+});
+
+
+
+
+
+
+
+
+
